3cb1e148da7e0005159129dc80887b41c78cb3e4
vinsonhwj <vinsonhwj@34f97ab2-3ea4-11de-b438-597f59cd7555>
2009-05-14 10:00:51 +0000

    增加类库

--- Object/NumberHelper.cs
+++ Object/NumberHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace hwj.CommonLibrary
+namespace hwj.CommonLibrary.Object
 {
     public class NumberHelper
     {
