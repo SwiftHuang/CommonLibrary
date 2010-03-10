129a6f0f61b87269dcb5067fedeed3f8c17dc133
vinsonhwj <vinsonhwj@34f97ab2-3ea4-11de-b438-597f59cd7555>
2010-03-10 10:49:01 +0000

    Temp:添加生成DLL

--- Object/WebServiceHelper.cs
+++ Object/WebServiceHelper.cs
@@ -22,80 +22,7 @@ namespace hwj.CommonLibrary.Object
         }
         public static object InvokeWebService(string url, string classname, string methodname, object[] args)
         {
-            return InvokeWebService(url, classname, methodname, args);
-        }
-        public static object InvokeWebService(string url, string classname, string methodname, string fileName, object[] args)
-        {
-
-            if ((classname == null) || (classname == ""))
-            {
-                classname = GetWsClassName(url);
-            }
-
-            try
-            {
-                //获取WSDL
-                using (WebClient wc = new WebClient())
-                {
-                    using (Stream stream = wc.OpenRead(url + "?WSDL"))
-                    {
-                        ServiceDescription sd = ServiceDescription.Read(stream);
-                        ServiceDescriptionImporter sdi = new ServiceDescriptionImporter();
-                        sdi.AddServiceDescription(sd, "", "");
-                        CodeNamespace cn = new CodeNamespace(@namespace);
-
-                        //生成客户端代理类代码
-                        CodeCompileUnit ccu = new CodeCompileUnit();
-                        ccu.Namespaces.Add(cn);
-                        sdi.Import(cn, ccu);
-                        CSharpCodeProvider csc = new CSharpCodeProvider();
-                        //ICodeCompiler icc = csc.CreateCompiler();
-
-                        //设定编译参数
-                        CompilerParameters cplist = new CompilerParameters();
-                        cplist.GenerateExecutable = false;
-                        cplist.GenerateInMemory = true;
-                        if (!string.IsNullOrEmpty(fileName))
-                            cplist.OutputAssembly = fileName;
-                        cplist.ReferencedAssemblies.Add("System.dll");
-                        cplist.ReferencedAssemblies.Add("System.XML.dll");
-                        cplist.ReferencedAssemblies.Add("System.Web.Services.dll");
-                        cplist.ReferencedAssemblies.Add("System.Data.dll");
-
-                        //编译代理类
-                        //CompilerResults cr = icc.CompileAssemblyFromDom(cplist, ccu);
-                        CompilerResults cr = csc.CompileAssemblyFromDom(cplist, ccu);
-                        if (true == cr.Errors.HasErrors)
-                        {
-                            System.Text.StringBuilder sb = new System.Text.StringBuilder();
-                            foreach (System.CodeDom.Compiler.CompilerError ce in cr.Errors)
-                            {
-                                sb.Append(ce.ToString());
-                                sb.Append(System.Environment.NewLine);
-                            }
-                            throw new Exception(sb.ToString());
-                        }
-
-                        //生成代理实例，并调用方法
-                        Assembly assembly = cr.CompiledAssembly;
-                        Type t = assembly.GetType(@namespace + "." + classname, true, true);
-                        object obj = Activator.CreateInstance(t);
-                        MethodInfo mi = t.GetMethod(methodname);
-
-                        if (args == null)
-                            return mi.Invoke(obj, null);
-                        else
-                            return mi.Invoke(obj, args);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                if (ex.InnerException != null)
-                    throw new Exception(ex.InnerException.Message, new Exception(ex.InnerException.StackTrace));
-                else
-                    throw ex;
-            }
+            return InvokeWebService(url, classname, methodname, args, null);
         }
         public static object InvokeWebServiceByDLL(string fileName, string url, string classname, string methodname, object[] args)
         {
@@ -124,10 +51,101 @@ namespace hwj.CommonLibrary.Object
                     throw ex;
             }
         }
-        public static bool CreateWebServiceDLL()
+        public static bool CreateWebServiceDLL(string url, string classname, string fileName)
         {
-            return false;
+            try
+            {
+                InvokeWebService(url, classname, null, null, fileName);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                return false;
+            }
         }
+        private static object InvokeWebService(string url, string classname, string methodname, object[] args, string fileName)
+        {
+
+            if ((classname == null) || (classname == ""))
+            {
+                classname = GetWsClassName(url);
+            }
+
+            try
+            {
+                //获取WSDL
+                WebClient wc = new WebClient();
+                Stream stream = wc.OpenRead(url + "?WSDL");
+
+                ServiceDescription sd = ServiceDescription.Read(stream);
+                ServiceDescriptionImporter sdi = new ServiceDescriptionImporter();
+                sdi.AddServiceDescription(sd, "", "");
+                CodeNamespace cn = new CodeNamespace(@namespace);
+
+                //生成客户端代理类代码
+                CodeCompileUnit ccu = new CodeCompileUnit();
+                ccu.Namespaces.Add(cn);
+                sdi.Import(cn, ccu);
+                CSharpCodeProvider csc = new CSharpCodeProvider();
+                //ICodeCompiler icc = csc.CreateCompiler();
+
+                //设定编译参数
+                CompilerParameters cplist = new CompilerParameters();
+                cplist.GenerateExecutable = false;
+
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    cplist.OutputAssembly = fileName;
+                    cplist.GenerateInMemory = false;
+                }
+                else
+                    cplist.GenerateInMemory = true;
+
+                cplist.ReferencedAssemblies.Add("System.dll");
+                cplist.ReferencedAssemblies.Add("System.XML.dll");
+                cplist.ReferencedAssemblies.Add("System.Web.Services.dll");
+                cplist.ReferencedAssemblies.Add("System.Data.dll");
+
+                //编译代理类
+                //CompilerResults cr = icc.CompileAssemblyFromDom(cplist, ccu);
+                CompilerResults cr = csc.CompileAssemblyFromDom(cplist, ccu);
+                if (true == cr.Errors.HasErrors)
+                {
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    foreach (System.CodeDom.Compiler.CompilerError ce in cr.Errors)
+                    {
+                        sb.Append(ce.ToString());
+                        sb.Append(System.Environment.NewLine);
+                    }
+                    throw new Exception(sb.ToString());
+                }
+
+                if (string.IsNullOrEmpty(fileName))
+                {
+                    //生成代理实例，并调用方法
+                    Assembly assembly = cr.CompiledAssembly;
+                    Type t = assembly.GetType(@namespace + "." + classname, true, true);
+                    object obj = Activator.CreateInstance(t);
+                    MethodInfo mi = t.GetMethod(methodname);
+
+                    if (args == null)
+                        return mi.Invoke(obj, null);
+                    else
+                        return mi.Invoke(obj, args);
+                }
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                    throw new Exception(ex.InnerException.Message, new Exception(ex.InnerException.StackTrace));
+                else
+                    throw ex;
+            }
+        }
+
         private static string GetWsClassName(string wsUrl)
         {
             string[] parts = wsUrl.Split('/');
