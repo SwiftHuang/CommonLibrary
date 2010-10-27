d305629e0f7f4917425435bacc5fa855b529e54f
gfull <gfull@qq.com>
2010-10-27 08:28:10 +0000

    整理内裤！

--- Object/EmailHelper.cs
+++ Object/EmailHelper.cs
@@ -17,26 +17,85 @@ namespace hwj.CommonLibrary.Object
         }
         public static bool Send(string smtpServer, string emailFrom, string emailFromPassword, string[] emailTo, string[] cc, string subject, string body, bool isBodyHtml, List<Attachment> attachments)
         {
-            SmtpInfos smtpInfos = TransToList(smtpServer, emailFrom, emailFromPassword);
+            SmtpInfos smtpInfos = new SmtpInfos(smtpServer, emailFrom, emailFromPassword);
             return Send(emailTo, cc, subject, body, isBodyHtml, attachments, ref smtpInfos);
         }
-        public static bool Send(string smtpServer, string emailFrom, string emailFromPassword, string[] emailTo, string[] cc, string subject, string body, bool isBodyHtml, List<StreamFile> streams, bool useGzip)
+        public static bool Send(string smtpServer, string emailFrom, string emailFromPassword, string[] emailTo, string[] cc, string subject, string body, bool isBodyHtml, List<StreamFile> streams)
         {
-            SmtpInfos smtpInfos = TransToList(smtpServer, emailFrom, emailFromPassword);
-            return Send(emailTo, cc, subject, body, isBodyHtml, streams, useGzip, ref smtpInfos);
+            SmtpInfos smtpInfos = new SmtpInfos(smtpServer, emailFrom, emailFromPassword);
+            return Send(emailTo, cc, subject, body, isBodyHtml, streams, ref smtpInfos);
         }
         public static bool Send(string smtpServer, string emailFrom, string emailFromPassword, MailMessage message, bool isBodyHtml)
         {
-            SmtpInfos smtpInfos = TransToList(smtpServer, emailFrom, emailFromPassword);
+            SmtpInfos smtpInfos = new SmtpInfos(smtpServer, emailFrom, emailFromPassword);
             return Send(message, isBodyHtml, ref smtpInfos);
         }
 
+
+        public static bool Send(string emailTo, string cc, string subject, string body, bool isBodyHtml, ref SmtpInfos smtpInfos)
+        {
+            return Send(emailTo, cc, subject, body, isBodyHtml, null, ref smtpInfos);
+        }
+        public static bool Send(string emailTo, string cc, string subject, string body, bool isBodyHtml, List<Attachment> attachments, ref SmtpInfos smtpInfos)
+        {
+            return Send(new string[] { emailTo }, new string[] { cc }, subject, body, isBodyHtml, attachments, ref  smtpInfos);
+        }
+        public static bool Send(string[] emailTo, string[] cc, string subject, string body, bool isBodyHtml, List<StreamFile> streams, ref SmtpInfos smtpInfos)
+        {
+            List<Attachment> attachments = new List<Attachment>();
+
+            if (streams != null && streams.Count > 0)
+            {
+                foreach (StreamFile s in streams)
+                {
+                    if (s.UseGzip)
+                        attachments.Add(new Attachment(hwj.CommonLibrary.Object.FileHelper.Stream2GzipStream(s.InStream), s.FileName + ".gz"));
+                    else
+                        attachments.Add(new Attachment(s.InStream, s.FileName));
+                }
+            }
+            return Send(emailTo, cc, subject, body, isBodyHtml, attachments, ref  smtpInfos);
+        }
+        public static bool Send(string[] emailTo, string[] cc, string subject, string body, bool isBodyHtml, List<Attachment> attachments, ref SmtpInfos smtpInfos)
+        {
+            MailMessage message = new MailMessage();
+            //message.From = new MailAddress(emailFrom);
+            message.Subject = subject;
+            message.Body = body;
+
+            if (emailTo != null)
+            {
+                foreach (string to in emailTo)
+                {
+                    if (!string.IsNullOrEmpty(to))
+                        message.To.Add(to);
+                }
+            }
+            if (cc != null)
+            {
+                foreach (string c in cc)
+                {
+                    if (!string.IsNullOrEmpty(c))
+                        message.CC.Add(c);
+                }
+            }
+
+            if (attachments != null && attachments.Count > 0)
+            {
+                foreach (Attachment a in attachments)
+                {
+                    message.Attachments.Add(a);
+                }
+            }
+            message.IsBodyHtml = isBodyHtml;
+            return Send(message, isBodyHtml, ref smtpInfos);
+        }
         public static bool Send(MailMessage message, bool isBodyHtml, ref SmtpInfos smtpInfos)
         {
             SmtpInfos streamlineList = new SmtpInfos();
             foreach (SmtpInfo smtpInfo in smtpInfos)
             {
-                if (smtpInfo.Active && !string.IsNullOrEmpty(smtpInfo.SmtpServer) && !string.IsNullOrEmpty(smtpInfo.EmailFrom) && smtpInfo.LastFailDate.Date < DateTime.Now.Date)
+                if (smtpInfo != null && smtpInfo.Active && !string.IsNullOrEmpty(smtpInfo.SmtpServer) && !string.IsNullOrEmpty(smtpInfo.EmailFrom) && smtpInfo.LastFailDate.Date < DateTime.Now.Date)
                 {
                     streamlineList.Add(smtpInfo);
                 }
@@ -74,76 +133,7 @@ namespace hwj.CommonLibrary.Object
             }
             return false;
         }
-        public static bool Send(string[] emailTo, string[] cc, string subject, string body, bool isBodyHtml, List<Attachment> attachments, ref SmtpInfos smtpInfos)
-        {
-            MailMessage message = new MailMessage();
-            //message.From = new MailAddress(emailFrom);
-            message.Subject = subject;
-            message.Body = body;
 
-            if (emailTo != null)
-            {
-                foreach (string to in emailTo)
-                {
-                    if (!string.IsNullOrEmpty(to))
-                        message.To.Add(to);
-                }
-            }
-            if (cc != null)
-            {
-                foreach (string c in cc)
-                {
-                    if (!string.IsNullOrEmpty(c))
-                        message.CC.Add(c);
-                }
-            }
-
-            if (attachments != null && attachments.Count > 0)
-            {
-                foreach (Attachment a in attachments)
-                {
-                    message.Attachments.Add(a);
-                }
-            }
-            message.IsBodyHtml = isBodyHtml;
-            return Send(message, isBodyHtml, ref smtpInfos);
-        }
-        public static bool Send(string[] emailTo, string[] cc, string subject, string body, bool isBodyHtml, List<StreamFile> streams, bool useGzip, ref SmtpInfos smtpInfos)
-        {
-            List<Attachment> attachments = new List<Attachment>();
-
-            if (streams != null && streams.Count > 0)
-            {
-                foreach (StreamFile s in streams)
-                {
-                    if (useGzip)
-                        attachments.Add(new Attachment(hwj.CommonLibrary.Object.FileHelper.Stream2GzipStream(s.InStream), s.FileName + ".gz"));
-                    else
-                        attachments.Add(new Attachment(s.InStream, s.FileName));
-                }
-            }
-            return Send(emailTo, cc, subject, body, isBodyHtml, attachments, ref  smtpInfos);
-        }
-        public static bool Send(string emailTo, string cc, string subject, string body, bool isBodyHtml, List<Attachment> attachments, ref SmtpInfos smtpInfos)
-        {
-            return Send(new string[] { emailTo }, new string[] { cc }, subject, body, isBodyHtml, attachments, ref  smtpInfos);
-        }
-        public static bool Send(string emailTo, string cc, string subject, string body, bool isBodyHtml, ref SmtpInfos smtpInfos)
-        {
-            return Send(emailTo, cc, subject, body, isBodyHtml, null, ref smtpInfos);
-        }
-        public static SmtpInfos TransToList(string smtpServer, string emailFrom, string emailFromPassword)
-        {
-            SmtpInfo smtpInfo = new SmtpInfo();
-            smtpInfo.SmtpServer = smtpServer;
-            smtpInfo.EmailFrom = emailFrom;
-            smtpInfo.EmailFromPassword = emailFromPassword;
-            smtpInfo.Active = true;
-            smtpInfo.LastFailDate = DateTime.MinValue;
-            SmtpInfos smtpInfos = new SmtpInfos();
-            smtpInfos.Add(smtpInfo);
-            return smtpInfos;
-        }
         public static bool isValidEmail(string xEmailAddress)
         {
             bool myIsEmail = false;
@@ -162,45 +152,79 @@ namespace hwj.CommonLibrary.Object
             {
                 if (x == null && y == null)
                     return 0;
-                if (x.LastFailDate == DateTime.MinValue)
-                {
-                    if (y.LastFailDate == DateTime.MinValue)
-                    {
-                        return 0;
-                    }
-                    else
-                    {
-                        return -1;
-                    }
-                }
-                else
-                {
-                    if (y.LastFailDate == DateTime.MinValue)
-                    {
-                        return 1;
-                    }
-                    else
-                    {
-                        return x.LastFailDate.CompareTo(y.LastFailDate);
-                    }
-                }
+                return x.LastFailDate.CompareTo(y.LastFailDate);
             }
         }
     }
     public class SmtpInfo
     {
-        public bool Active = false;
-        public string SmtpServer = string.Empty;
-        public string EmailFrom = string.Empty;
-        public string EmailFromPassword = string.Empty;
-        public DateTime LastFailDate = DateTime.MinValue;
-        public Exception Exception = null;
-    }
-    public class SmtpInfos : List<SmtpInfo> { }
+        public SmtpInfo() { }
+        public SmtpInfo(string smtpServer, string emailFrom)
+        {
+            new SmtpInfo(smtpServer, emailFrom, string.Empty);
+        }
 
+        public SmtpInfo(string smtpServer, string emailFrom, string emailFromPassword)
+        {
+            this.Active = true;
+            this.SmtpServer = smtpServer;
+            this.EmailFrom = emailFrom;
+            this.EmailFromPassword = emailFromPassword;
+            this.LastFailDate = DateTime.MinValue;
+        }
+
+        public bool Active { get; set; }
+        public string SmtpServer { get; set; }
+        public string EmailFrom { get; set; }
+        public string EmailFromPassword { get; set; }
+        public DateTime LastFailDate { get; set; }
+        public Exception Exception { get; set; }
+    }
+
+    public class SmtpInfos : List<SmtpInfo>
+    {
+        public SmtpInfos() { }
+        public SmtpInfos(string smtpServer, string emailFrom, string emailFromPassword)
+        {
+            SmtpInfo smtpInfo = new SmtpInfo(smtpServer, emailFrom, emailFromPassword);
+            this.Add(smtpInfo);
+        }
+    }
+    
+    /// <summary>
+    ///
+    /// </summary>
     public class StreamFile
     {
-        public Stream InStream;
-        public string FileName = string.Empty;
+        public StreamFile() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StreamFile"/> class,default translate into GzipStream .
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <param name="inStream">The in stream.</param>
+        public StreamFile(string fileName, Stream inStream)
+        {
+            new StreamFile(fileName, inStream, true);
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StreamFile"/> class.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <param name="inStream">The in stream.</param>
+        /// <param name="useGzip">if set to <c>true</c> [use gzip].</param>
+        public StreamFile(string fileName, Stream inStream, bool useGzip)
+        {
+            this.FileName = fileName;
+            this.InStream = inStream;
+            this.UseGzip = useGzip;
+        }
+
+        /// <summary>
+        /// Get or Set the value to control either use Gzip to compress the stream or not
+        /// 
+        /// </summary>
+        public bool UseGzip { get; set; }
+        public Stream InStream { get; set; }
+        public string FileName { get; set; }
     }
 }
