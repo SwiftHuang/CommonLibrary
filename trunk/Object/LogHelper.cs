e09f8badf8d3169680ee7873feed8c56ad64e730
vinsonhwj@gmail.com <vinsonhwj@gmail.com@34f97ab2-3ea4-11de-b438-597f59cd7555>
2011-06-24 06:42:47 +0000

    重新整理LogHelper

--- trunk/Object/LogHelper.cs
+++ trunk/Object/LogHelper.cs
@@ -7,78 +7,61 @@ namespace hwj.CommonLibrary.Object
     public class LogHelper : Base.LogHelper
     {
         public LogHelper()
+            : this(string.Empty)
         {
-            base.Initialization(null);
+
         }
         public LogHelper(string fileName)
+            : base(fileName)
         {
-            base.Initialization(fileName);
+
         }
 
-        #region Info
-        public void Info(string log, Exception ex, string EmailSubject, bool sendEmail)
-        {
-            base.InfoAction(log, ex, EmailSubject, sendEmail);
-        }
-        public void Info(string log, Exception ex, string EmailSubject, string emailTo, string emailCC)
-        {
-            base.InfoAction(log, ex, EmailSubject, true, emailTo, emailCC);
-        }
-        public void Info(string log, Exception ex, string EmailSubject, string emailTo, string emailCC, List<Attachment> attachments)
-        {
-            base.InfoAction(log, ex, EmailSubject, true, emailTo, emailCC, attachments);
-        }
-        #endregion
+        //#region Info
+        //public void Info(string log, Exception ex, string emailSubject)
+        //{
+        //    base.InfoWithEmail(log, ex, emailSubject);
+        //}
+        //public void Info(string log, Exception ex, string emailSubject, string emailTo, string emailCC)
+        //{
+        //    base.InfoAction(log, ex, emailSubject, emailTo, emailCC);
+        //}
+        //public void Info(string log, Exception ex, string emailSubject, string emailTo, string emailCC, List<Attachment> attachments)
+        //{
+        //    base.InfoAction(log, ex, emailSubject, emailTo, emailCC, attachments);
+        //}
+        //#endregion
 
-        #region Warn
-        public void Warn(string log, Exception ex, string EmailSubject)
-        {
-            base.WarnAction(log, ex, EmailSubject);
-        }
-        public void Warn(string log, Exception ex, string EmailSubject, string emailTo, string emailCC)
-        {
-            base.WarnAction(log, ex, EmailSubject, emailTo, emailCC);
-        }
-        public void Warn(string log, Exception ex, string EmailSubject, string emailTo, string emailCC, List<Attachment> attachments)
-        {
-            base.WarnAction(log, ex, EmailSubject, emailTo, emailCC, attachments);
-        }
-        #endregion
+        //#region Warn
+        //public void Warn(string log, Exception ex, string emailSubject)
+        //{
+        //    base.WarnWithEmail(log, ex, emailSubject);
+        //}
+        //public void Warn(string log, Exception ex, string emailSubject, string emailTo, string emailCC)
+        //{
+        //    base.WarnAction(log, ex, emailSubject, emailTo, emailCC);
+        //}
+        //public void Warn(string log, Exception ex, string emailSubject, string emailTo, string emailCC, List<Attachment> attachments)
+        //{
+        //    base.WarnAction(log, ex, emailSubject, emailTo, emailCC, attachments);
+        //}
+        //#endregion
 
-        #region Error
-        public void Error(string log, Exception ex, string EmailSubject)
-        {
-            base.ErrorAction(log, ex, EmailSubject);
-        }
-        public void Error(string log, Exception ex, string EmailSubject, string emailTo, string emailCC)
-        {
-            base.ErrorAction(log, ex, EmailSubject, emailTo, emailCC);
-        }
-        public void Error(string log, Exception ex, string EmailSubject, string emailTo, string emailCC, List<Attachment> attachments)
-        {
-            base.ErrorAction(log, ex, EmailSubject, emailTo, emailCC, attachments);
-        }
-        #endregion
+        //#region Error
+        //public void Error(string log, Exception ex, string emailSubject)
+        //{
+        //    base.ErrorWithEmail(log, ex, emailSubject);
+        //}
+        //public void Error(string log, Exception ex, string emailSubject, string emailTo, string emailCC)
+        //{
+        //    base.ErrorAction(log, ex, emailSubject, emailTo, emailCC);
+        //}
+        //public void Error(string log, Exception ex, string emailSubject, string emailTo, string emailCC, List<Attachment> attachments)
+        //{
+        //    base.ErrorAction(log, ex, emailSubject, emailTo, emailCC, attachments);
+        //}
+        //#endregion
 
-        /// <summary>
-        /// 获取当前系统、版本、版本号
-        /// </summary>
-        /// <returns></returns>
-        public static string GetOSVersion()
-        {
-            try
-            {
-                StringBuilder sb = new StringBuilder();
-                string informaction = Environment.OSVersion.VersionString.ToString();
-                string version = Environment.Version.ToString();
 
-                sb.AppendFormat("{0}({1})", informaction, version);
-                return sb.ToString();
-            }
-            catch
-            {
-                return "Failed to get system information";
-            }
-        }
     }
 }
