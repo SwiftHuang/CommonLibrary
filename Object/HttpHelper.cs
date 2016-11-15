6cc4a234cd2fb7e03686a163167256ea1ec1ea80
Vinson WM <vinsonhwj@gmail.com>
2016-11-15 09:18:56 +0800

    整理HttpHelper

--- Object/HttpHelper.cs
+++ Object/HttpHelper.cs
@@ -1,25 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
 
 namespace hwj.CommonLibrary.Object
 {
+    /// <summary>
+    /// HttpHelper
+    /// </summary>
     public class HttpHelper
     {
+        #region Property
+
         //private const string GetContentType = "text/plain";
         private const int defaultTimeOut = 30000;
 
-        private const string PostContentType = "application/x-www-form-urlencoded";
-        private const string Get = "GET";
+        private const string postContentType = "application/x-www-form-urlencoded";
 
+        #endregion Property
+
+        #region Public Function
+
+        /// <summary>
+        /// 使用Post的方式提交数据（Timeout默认30秒）
+        /// </summary>
+        /// <param name="url">提交Url</param>
+        /// <param name="param">提交参数</param>
+        /// <param name="encoding">字符编码</param>
+        /// <returns></returns>
+        public static string PostAction(string url, string param, Encoding encoding)
+        {
+            return PostAction(url, param, encoding);
+        }
+
+        /// <summary>
+        /// 使用Post的方式提交数据（Timeout默认30秒）
+        /// </summary>
+        /// <param name="url">提交Url</param>
+        /// <param name="param">提交参数</param>
+        /// <param name="encoding">字符编码</param>
+        /// <param name="timeout">超时时间</param>
+        /// <returns></returns>
         public static string PostAction(string url, string param, Encoding encoding, int timeout)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Timeout = timeout > defaultTimeOut ? timeout : defaultTimeOut;
             request.Method = "POST";
-            request.ContentType = PostContentType;
+            request.ContentType = postContentType;
 
             Stream dataStream = request.GetRequestStream();
             byte[] bytes = DataToBytes(param, encoding);
@@ -31,76 +58,24 @@ namespace hwj.CommonLibrary.Object
             return rs;
         }
 
-        public static string GetAction(string url, Dictionary<string, string> data)
-        {
-            if (data != null && data.Count > 0)
-            {
-                url = CombineQueryUrl(url, data);
-            }
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            //request.Timeout = timeOut;
-            //request.ContentType = GetContentType;
-            request.Method = "GET";
-            string rs = GetResponeString(request);
-            return rs;
-        }
+        //public static string GetAction(string url, Dictionary<string, string> data)
+        //{
+        //    if (data != null && data.Count > 0)
+        //    {
+        //        url = CombineQueryUrl(url, data);
+        //    }
+        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        //    //request.Timeout = timeOut;
+        //    //request.ContentType = GetContentType;
+        //    request.Method = "GET";
+        //    string rs = GetResponeString(request);
+        //    return rs;
+        //}
 
-        private static string GetResponeString(HttpWebRequest rq)
-        {
-            HttpWebResponse httpWebResponse = (HttpWebResponse)rq.GetResponse();
-            using (Stream responseStream = httpWebResponse.GetResponseStream())
-            using (StreamReader sr = new StreamReader(responseStream))
-            {
-                string str = sr.ReadToEnd();
-                return str;
-            }
-        }
-
-        private static string CombineQueryUrl(string url, Dictionary<string, string> data)
-        {
-            string query = GetFormatedData(data);
-            if (!url.Contains("?"))
-            {
-                url += "?" + query;
-            }
-            else
-            {
-                url += "&" + query;
-            }
-            return url;
-        }
-
-        private static string GetFormatedData(Dictionary<string, string> data)
-        {
-            StringBuilder sb = new StringBuilder(200);
-            if (data != null && data.Count > 0)
-            {
-                foreach (var i in data)
-                {
-                    sb.AppendFormat("{0}={1}&", i.Key, i.Value);
-                }
-            }
-            string rs = sb.ToString();
-            if (!string.IsNullOrEmpty(rs))
-            {
-                rs = rs.TrimEnd('&');
-            }
-            return rs;
-        }
-
-        private static byte[] DataToBytes(string param, Encoding encoding)
-        {
-            byte[] bytes = new byte[0];
-            bytes = encoding.GetBytes(param);
-            return bytes;
-        }
-
-        private static byte[] DataToBytes(Dictionary<string, string> data, Encoding encoding)
-        {
-            string param = GetFormatedData(data);
-            return DataToBytes(param, encoding);
-        }
-
+        /// <summary>
+        /// 获取IP地址
+        /// </summary>
+        /// <returns></returns>
         public static string GetIP()
         {
             string IP4Address = String.Empty;
@@ -130,5 +105,66 @@ namespace hwj.CommonLibrary.Object
 
             return IP4Address;
         }
+
+        #endregion Public Function
+
+        #region Private Function
+
+        private static byte[] DataToBytes(string param, Encoding encoding)
+        {
+            byte[] bytes = new byte[0];
+            bytes = encoding.GetBytes(param);
+            return bytes;
+        }
+
+        private static string GetResponeString(HttpWebRequest rq)
+        {
+            HttpWebResponse httpWebResponse = (HttpWebResponse)rq.GetResponse();
+            using (Stream responseStream = httpWebResponse.GetResponseStream())
+            using (StreamReader sr = new StreamReader(responseStream))
+            {
+                string str = sr.ReadToEnd();
+                return str;
+            }
+        }
+
+        //private static string CombineQueryUrl(string url, Dictionary<string, string> data)
+        //{
+        //    string query = GetFormatedData(data);
+        //    if (!url.Contains("?"))
+        //    {
+        //        url += "?" + query;
+        //    }
+        //    else
+        //    {
+        //        url += "&" + query;
+        //    }
+        //    return url;
+        //}
+
+        //private static string GetFormatedData(Dictionary<string, string> data)
+        //{
+        //    StringBuilder sb = new StringBuilder(200);
+        //    if (data != null && data.Count > 0)
+        //    {
+        //        foreach (var i in data)
+        //        {
+        //            sb.AppendFormat("{0}={1}&", i.Key, i.Value);
+        //        }
+        //    }
+        //    string rs = sb.ToString();
+        //    if (!string.IsNullOrEmpty(rs))
+        //    {
+        //        rs = rs.TrimEnd('&');
+        //    }
+        //    return rs;
+        //}
+        //private static byte[] DataToBytes(Dictionary<string, string> data, Encoding encoding)
+        //{
+        //    string param = GetFormatedData(data);
+        //    return DataToBytes(param, encoding);
+        //}
+
+        #endregion Private Function
     }
 }
