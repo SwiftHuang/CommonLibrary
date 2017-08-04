07348ae54915759969de1398df427b8eb79cd125
Vinson WM <vinsonhwj@qq.com>
2017-08-04 15:35:11 +0800

    扩展方法增加返回string.empty

--- Object/Extensions/ExStringHelper.cs
+++ Object/Extensions/ExStringHelper.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace hwj.CommonLibrary.Object.Extensions
+﻿namespace hwj.CommonLibrary.Object.Extensions
 {
     public static class ExStringHelper
     {
@@ -19,6 +15,21 @@ namespace hwj.CommonLibrary.Object.Extensions
             }
             return str.ToUpper().Trim();
         }
+
+        /// <summary>
+        /// 从当前 System.String 对象的开始和末尾移除所有空白字符并转大写，如Null则返回string.Empty。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToUpperTrimEmpty(this string str)
+        {
+            if (str == null)
+            {
+                return string.Empty;
+            }
+            return str.ToUpper().Trim();
+        }
+
         /// <summary>
         /// 从当前 System.String 对象的开始和末尾移除所有空白字符并转小写，如Null则返回Null。
         /// </summary>
@@ -34,6 +45,20 @@ namespace hwj.CommonLibrary.Object.Extensions
         }
 
         /// <summary>
+        /// 从当前 System.String 对象的开始和末尾移除所有空白字符并转小写，如Null则返回string.Empty。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToLowerTrimEmpty(this string str)
+        {
+            if (str == null)
+            {
+                return string.Empty;
+            }
+            return str.ToLower().Trim();
+        }
+
+        /// <summary>
         /// 从当前 System.String 对象的开始和末尾移除所有空白字符，如Null则返回Null。
         /// </summary>
         /// <param name="str"></param>
@@ -43,6 +68,20 @@ namespace hwj.CommonLibrary.Object.Extensions
             if (str == null)
             {
                 return str;
+            }
+            return str.Trim();
+        }
+
+        /// <summary>
+        /// 从当前 System.String 对象的开始和末尾移除所有空白字符，如Null则返回string.Empty。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string TryTrimEmpty(this string str)
+        {
+            if (str == null)
+            {
+                return string.Empty;
             }
             return str.Trim();
         }
